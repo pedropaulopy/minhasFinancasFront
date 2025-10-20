d50fc90553f68564d90ab1897ed1d91bb30da06e
Pedro Paulo <pedro.carlo@muralis.com.br>
2025-10-20 15:19:48 -0300

    Implementa uma DialogBox para confirmação de deleção do lançamento selecionado.

--- minhas-financas-app/src/main/App.jsx
+++ minhas-financas-app/src/main/App.jsx
@@ -1,9 +1,10 @@
 import Rotas from "./rotas";
 import Navbar from "../components/navbar";
 import React from 'react'
-
 import 'toastr/build/toastr.min.css'
 
+import 'primereact/resources/themes/nova/theme.css'
+import 'primereact/resources/primereact.min.css'
 import 'toastr/build/toastr.css'
 import '../minhas-financas-template/html/Bootswatch_ Flatly_files/bootstrap.css'
 import '../custom.css'
--- minhas-financas-app/src/views/lancamentos/consultaLancamentos.jsx
+++ minhas-financas-app/src/views/lancamentos/consultaLancamentos.jsx
@@ -5,15 +5,19 @@ import SelectMenu from "../../components/selectMenu";
 import LancamentosTable from "./lancamentosTable";
 import LancamentoService from "../../app/services/lancamentoService"
 import {mensagemErro, mensagemSucesso} from '../../components/toastr'
-
+import {Dialog} from 'primereact/dialog'
+import {Button} from "primereact/button";
 
 function ConsultaLancamentos(){
     const [ano, setAno] = useState();
     const [mes, setMes] = useState();
     const [tipo, setTipo] = useState();
     const [descricao, setDescricao] = useState();
+    const [showConfirmDialog, setShowConfirmDialog] = useState();
     const [lancamentos, setLancamentos] = useState([]);
+    const [lancamentoDeletar, setLancamentoDeletar] = useState({});
     const service = new LancamentoService();
+
 
     const listaMeses = service.obterListaMeses()
     const listaTipos = service.obterListaTipos()
@@ -47,16 +51,34 @@ function ConsultaLancamentos(){
         service.put(id)
     }
 
-    const deletar = (lancamento) => {
-        service.deletar(lancamento.id).then(response => {
-            const novosLancamentos = lancamentos.filter(l => l.id !== lancamento.id);
+    const deletar = () => {
+        service.deletar(lancamentoDeletar.id).then(response => {
+            const novosLancamentos = lancamentos.filter(l => l.id !== lancamentoDeletar.id);
             setLancamentos(novosLancamentos);
+            setShowConfirmDialog(false)
             mensagemSucesso("Sucesso!", "Lançamento deletado com sucesso");
         }).catch(error => {
             mensagemErro("Erro", "Ocorreu um erro ao tentar deletar o lançamento.");
             console.error(error);
         });
     }
+
+    const abrirConfirmacao = (lancamento) => {
+        setShowConfirmDialog(true)
+        setLancamentoDeletar(lancamento)
+    }
+
+    const fecharConfirmacao = () => {
+        setShowConfirmDialog(false)
+        setLancamentoDeletar({})
+    }
+
+    const confirmDialogFooter = (
+        <div>
+            <Button label={"Confirmar"} icon={"pi pi-check"} onClick={deletar}/>
+            <Button label={"Cancelar"} icon={"pi pi-times"} onClick={fecharConfirmacao} className={"p-button-secondary"}/>
+        </div>
+    )
 
     return(
         <Card title={"Consulta Lançamentos"}>
@@ -94,7 +116,7 @@ function ConsultaLancamentos(){
                             />
                         </FormGroup>
                         <button type={"button"} className={"btn btn-success"} onClick={buscar}>Buscar</button>
-                        <button type={"button"} className={"btn btn-danger"} >Cadastrar</button>
+                        <button type={"button"} className={"btn btn-danger"}>Cadastrar</button>
                     </div>
                 </div>
             </div>
@@ -102,9 +124,14 @@ function ConsultaLancamentos(){
             <div className={"row"}>
                 <div className={"col-md-12"}>
                     <div className={"bs-component"}>
-                        <LancamentosTable Lancamentos={lancamentos} deleteAction={deletar} editAction={editar}/>
+                        <LancamentosTable Lancamentos={lancamentos} deleteAction={abrirConfirmacao} editAction={editar}/>
                     </div>
                 </div>
+            </div>
+            <div>
+                <Dialog header={"Deletar Lançamento"} footer={confirmDialogFooter} visible={showConfirmDialog} style={{width: '50vw'}} modal={true} onHide={() => setShowConfirmDialog(false)}>
+                    Deseja deletar esse lançamento?
+                </Dialog>
             </div>
         </Card>
     )
